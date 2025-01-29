f99aa0c290e74b8ab4f135d2f2ba7d78fdacf70f
hansganzgeheim <stefan.seisenbacher@gmx.at>
2025-01-29 14:20:07 +0100

    Adds input field for longitude and latitude

--- frontend/app/(tabs)/admin/location/editLocation.tsx
+++ frontend/app/(tabs)/admin/location/editLocation.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { Button, useTheme } from 'react-native-paper';
-import { View, Text, TextInput, StyleSheet, Modal, Pressable } from 'react-native';
+import { Button, useTheme, TextInput } from 'react-native-paper';
+import { View, Text, StyleSheet, Modal, Pressable } from 'react-native';
 import { useNavigation, useLocalSearchParams, router } from "expo-router";
 import LocationService from '../../../service/location.service';
 import { Location } from '../../../models/event';
@@ -13,8 +13,16 @@ export default function EditEvent() {
     const [location, setLocation] = useState<Location>();
     const [name, setName] = useState('');
     const [website, setWebsite] = useState('');
+    const [longitude, setLongitude] = useState<Number>();
+    const [latitude, setLatitude] = useState<Number>();
+
     const [loading, setLoading] = useState(true);
     const { getLocationById, createLocation, updateLocation } = LocationService();
+
+    useEffect(() => {
+
+
+    }, []);
 
     useLayoutEffect(() => {
         let title = id == undefined ? "Create location" : "Edit location";
@@ -29,6 +37,8 @@ export default function EditEvent() {
                     setLocation(response.data);
                     setName(response.data.name);
                     setWebsite(response.data.website);
+                    setLongitude(response.data.longitude);
+                    setLatitude(response.data.latitude);
                     setLoading(false);
                 })
                 .catch(error => {
@@ -78,24 +88,44 @@ export default function EditEvent() {
     return (
         <View style={styles.container}>
             <View style={styles.form}>
-                <View style={styles.inputRow}>
-                    <Text style={styles.label}>Name:</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={name}
-                        onChangeText={setName}
-                        placeholder="Enter name"
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text style={styles.label}>Description:</Text>
-                    <TextInput
-                        style={styles.input}
-                        value={website}
-                        onChangeText={setWebsite}
-                        placeholder="Enter website"
-                    />
-                </View>
+                <TextInput
+                    mode='outlined'
+                    label="Name"
+                    style={styles.input}
+                    value={name}
+                    onChangeText={setName}
+                    placeholder="Enter name"
+                />
+                <TextInput
+                    mode='outlined'
+                    label="Latitude"
+                    style={styles.input}
+                    value={latitude?.toString()}
+                    onChangeText={(value) => {
+                        const numericValue = value.replace(/[^0-9.]/g, "");
+                        setLatitude(Number(numericValue));
+                    }}
+                    placeholder="Enter Latitude"
+                />
+                <TextInput
+                    mode='outlined'
+                    label="Longitude"
+                    style={styles.input}
+                    value={longitude?.toString()}
+                    onChangeText={(value) => {
+                        const numericValue = value.replace(/[^0-9.]/g, "");
+                        setLongitude(Number(numericValue));
+                    }}
+                    placeholder="Enter Longitude"
+                />
+                <TextInput
+                    mode='outlined'
+                    label="Website"
+                    style={styles.input}
+                    value={website}
+                    onChangeText={setWebsite}
+                    placeholder="Enter website"
+                />
                 <View style={styles.buttonRow}>
                     <Button mode="contained" onPress={handleSave} style={styles.actionButton} >Save</Button>
                     <Button mode="outlined" onPress={() => navigation.goBack()} style={styles.actionButton} >
@@ -110,40 +140,20 @@ export default function EditEvent() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-
         alignItems: 'center',
         padding: 20,
         backgroundColor: '#f5f5f5',
     },
-    title: {
-        fontSize: 24,
-        textAlign: 'center',
-        marginBottom: 20,
-    },
+
     form: {
+        flex: 1,
         width: '100%',
-        marginTop: 40,
-        maxWidth: 400,
-    },
-    inputRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 15,
-    },
-    label: {
-        width: '30%',
-        fontSize: 16,
-        textAlign: 'right',
-        marginRight: 10,
+        marginTop: 5,
+        maxWidth: 600,
     },
     input: {
-        width: '65%',
-        borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 10,
-        borderRadius: 5,
-        backgroundColor: '#fff',
+        width: "100%",
+        marginBottom: 10
     },
     buttonRow: {
         flexDirection: 'row',
