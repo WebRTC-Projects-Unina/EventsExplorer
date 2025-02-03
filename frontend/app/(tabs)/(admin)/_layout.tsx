fe01ab2df58f92c1ad856483296538a79f28df07
hansganzgeheim <stefan.seisenbacher@gmx.at>
2025-02-03 10:57:19 +0100

    Moves router.replace() to useEffect to avoid error

--- frontend/app/(tabs)/(admin)/_layout.tsx
+++ frontend/app/(tabs)/(admin)/_layout.tsx
@@ -2,14 +2,19 @@ import { Text } from 'react-native';
 import { useSession } from '@/hooks/authProvider';
 import { Slot } from 'expo-router';
 import { router } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function TestLayout() {
     const { session, isLoading } = useSession();
+
+    useEffect(() => {
+        if (!session) {
+            router.replace("/(tabs)/sign-in");
+        }
+    }, [session]);
+
     if (isLoading) {
         return <Text>Loading...</Text>;
-    }
-    if (!session) {
-        router.replace("/(tabs)/sign-in");
     }
 
     return (
